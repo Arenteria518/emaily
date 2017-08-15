b7f9a50876b59bb10f1c6611644fd5164965ecd3
Anthony Renteria <Anthony.Renteria@glg.com>
2017-08-15 14:54:44 -0600

    adding mongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,24 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL:'/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log("accessToken:", accessToken);
-        console.log("refresh Token:", refreshToken);
-        console.log("profile,", profile);
+    },
+    (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId:profile.id })
+            .then((existingUser) =>{
+                if(existingUser) {
+
+                } else{
+                    new User({googleId: profile.id}).save();
+                }
+            });
     })
 );
