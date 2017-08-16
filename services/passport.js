3ff88dd57ce80e01e37e1f5076364a2b677bf88b
Anthony Renteria <Anthony.Renteria@glg.com>
2017-08-16 17:18:25 -0600

    adding prod keys

--- services/passport.js
+++ services/passport.js
@@ -5,20 +5,37 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user =>{
+            done(null, user);
+        })
+});
+
 passport.use(
     new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL:'/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId:profile.id })
-            .then((existingUser) =>{
-                if(existingUser) {
-
-                } else{
-                    new User({googleId: profile.id}).save();
-                }
-            });
-    })
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({googleId: profile.id})
+                .then((existingUser) => {
+                    if (existingUser) {
+                        //we already have a record for the given profile ID
+                        done(null, existingUser);
+                    } else {
+                        //no record exists, create a new one
+                        new User({googleId: profile.id})
+                            .save()
+                            .then(user => {
+                                done(null, user);
+                            })
+                    }
+                });
+        })
 );
