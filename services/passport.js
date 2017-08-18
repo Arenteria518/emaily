5b13e98d6413f920fa77afb8d0f35eb63d303938
Anthony.Renteria <Anthony.Renteria@glg.com>
2017-08-18 17:29:56 -0600

    setting up the client

--- services/passport.js
+++ services/passport.js
@@ -9,11 +9,9 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user =>{
-            done(null, user);
-        })
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id)
+    done(null, user);
 });
 
 passport.use(
@@ -21,22 +19,15 @@ passport.use(
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
-            proxy:true
+            proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id})
-                .then((existingUser) => {
-                    if (existingUser) {
-                        //we already have a record for the given profile ID
-                        done(null, existingUser);
-                    } else {
-                        //no record exists, create a new one
-                        new User({googleId: profile.id})
-                            .save()
-                            .then(user => {
-                                done(null, user);
-                            })
-                    }
-                });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleId: profile.id})
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
+
         })
 );
