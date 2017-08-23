93379a24bd6886666440dfa5071632533d18111c
Anthony.Renteria <Anthony.Renteria@glg.com>
2017-08-22 21:07:08 -0600

    finished auth header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions'
 
-const App = () => {
-  return(
-      <div>
-          Hi there!
-      </div>
-  )
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => <h2>DashBoard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+class App extends Component{
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Header/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                        <Route path="/surveys" component={Dashboard}/>
+                        <Route path="/" component={Landing}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        )
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
